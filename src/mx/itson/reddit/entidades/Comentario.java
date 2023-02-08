7f4df6f1c0ab9a3841c779a921cee922820649b2
Jesus240628 <jesus.quintero240628@potros.itson.edu.mx>
2023-02-07 23:01:38 -0700

    Creacion de paquetes y clases

---
+++ src/mx/itson/reddit/entidades/Comentario.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.reddit.entidades;
+
+/**
+ *
+ * @author Jesus Javier Quintero Fierro-240628
+ */
+public class Comentario {
+    
+}
